488e4362d658134fccbc3f2ea59df17b08519bba
sampalm <samuelpalmeira@outlook.com>
2018-06-21 12:00:59 -0300

    adiciona model bean funcionario

---
+++ PI/Padaria/src/model/bean/Funcionario.java
@@ -0,0 +1,42 @@
+package model.bean;
+
+public class Funcionario {
+    private int id;
+    private String nome;
+    private String setor;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+    
+}
