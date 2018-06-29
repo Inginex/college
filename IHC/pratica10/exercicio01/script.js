06bd5b55c39de1a6882b5e2c29f4bf6be1877484
sampalm <samuelpalmeira@outlook.com>
2018-06-29 12:26:20 -0300

    atualiza exercicio 01

--- IHC/pratica10/exercicio01/script.js
+++ IHC/pratica10/exercicio01/script.js
@@ -1,5 +1,24 @@
-const trs = document.querySelectorAll("tr td:first-child");
-trs.forEach((td) => {
-    td.title = "Nome do Aluno";
+const trf = document.querySelectorAll("tr td:first-child");
+const trl = document.querySelectorAll("tr td:last-child");
+const trn = document.querySelectorAll("tr td:nth-child(5)");
+const mensagem = document.getElementById("mensagem");
+
+trf.forEach((td) => {
+    td.onclick = function() {
+        mensagem.style.display = "block";
+        mensagem.innerHTML = "<p>Nome do Aluno!</p>"
+    }
+})
+trn.forEach((td) => {
+    td.onclick = function() {
+        mensagem.style.display = "block";
+        mensagem.innerHTML = "<p>Nota total do aluno!</p>"
+    }
+})
+trl.forEach((td) => {
+    td.onclick = function() {
+        mensagem.style.display = "block";
+        mensagem.innerHTML = "<p>Quantidade de faltas!</p>"
+    }
 })
 
